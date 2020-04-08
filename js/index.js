2175c1b45f0324d9ec5511236ac899d598760ce6
LvyHuang <1903757279@qq.com>
2020-04-09 01:09:42 +0800

    增加了按钮

--- js/index.js
+++ js/index.js
@@ -1,59 +1,30 @@
+
+
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 var using = false;
 var lastPoint = {x:undefined, y:undefined};
 var eraser = document.getElementById("eraser");
 var eraserEnabled = false;
-getViewport();
+var brush = document.getElementById("brush");
+var actions = document.getElementById("actions");
 
-window.onresize = function(){
-    getViewport();
-};
-//鼠标按下时
-canvas.onmousedown = function (a) {
-    using = true;
-    var x = a.clientX;
-    var y = a.clientY;
-    lastPoint = {x:x, y:y};
-};
-//鼠标移动时
-canvas.onmousemove = function (a) {
-    var x = a.clientX;
-    var y = a.clientY;
+autoCanvasSize(canvas);
+listenToMouse(canvas);
+
+brush.onclick = function () {
     if(eraserEnabled){
-        if(using){
-            context.clearRect(x-5, y-5, 10, 10);
-        }
-    }else{
-        if(using){
-            var newPoint = {x:x, y:y};
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-            lastPoint = newPoint;
-        }
+        eraserEnabled = false;
+        actions.className = "actions";
     }
-
-};
-//鼠标松开时
-canvas.onmouseup = function () {
-    using = false;
 };
 eraser.onclick = function () {
-    eraserEnabled = !eraserEnabled;
-    eraser.style.background = eraserEnabled ? 'white':'#ccc';
+    if(!eraserEnabled) {
+        eraserEnabled = true;
+        actions.className = "actions x";
+    }
 };
-function getViewport() {
-    var pageWidth = document.documentElement.clientWidth;
-    var pageHeight = document.documentElement.clientHeight;
-    canvas.width = pageWidth;
-    canvas.height = pageHeight;
-}
-//画圈
-function drawCircle(x, y) {
-    context.beginPath();
-    context.fillStyle = "red";
-    context.arc(x, y, 1, 0, 2 * Math.PI);
-    context.fill();
-}
+
 //连线
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
@@ -62,4 +33,48 @@ function drawLine(x1, y1, x2, y2) {
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
+}
+//全屏
+function autoCanvasSize(canvas) {
+    setCanvasSize();
+    window.onresize = function(){
+        setCanvasSize();
+    };
+    function setCanvasSize() {
+        var pageWidth = document.documentElement.clientWidth;
+        var pageHeight = document.documentElement.clientHeight;
+        canvas.width = pageWidth;
+        canvas.height = pageHeight;
+    }
+}
+
+
+function listenToMouse(canvas){
+    //鼠标按下时
+    canvas.onmousedown = function (a) {
+        using = true;
+        var x = a.clientX;
+        var y = a.clientY;
+        lastPoint = {x:x, y:y};
+    };
+    //鼠标移动时
+    canvas.onmousemove = function (a) {
+        var x = a.clientX;
+        var y = a.clientY;
+        if(eraserEnabled){
+            if(using){
+                context.clearRect(x-5, y-5, 10, 10);
+            }
+        }else{
+            if(using){
+                var newPoint = {x:x, y:y};
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                lastPoint = newPoint;
+            }
+        }
+    };
+    //鼠标松开时
+    canvas.onmouseup = function () {
+        using = false;
+    };
 }
